4e7046868e350575e03f423240f03884060d0eff
Willem Yarbrough <yarbroughw@allegheny.edu>
2015-03-25 14:49:05 -0400

    refactored scout's square spiral generator. now completes full square

--- src/main/scala/edu/allegheny/beecolony/Scout.scala
+++ src/main/scala/edu/allegheny/beecolony/Scout.scala
@@ -8,11 +8,12 @@ import java.net.ServerSocket
  */
 object Scout extends App with Robot {
   val server = new ServerSocket(port)
-  val increment = 2 // change this to increase the width by which the loop increases
   val moves: Stream[Coordinate] = {
+    val scale = 8.0   // inches between points
     def loop(i: Int): Stream[Coordinate] =
-      (i * 1,i * 1)   #:: (i * -1,i * 1)  #::
-      (i * 1,i * -1)  #:: (i * -1,i * -1) #:: loop(i + increment)
+      val x = i * scale
+      (x *  1,x *  1) #:: (x * -1,x * 1) #:: (x * 1,x * -1) #::
+      (x * -1,x * -1) #:: (x *  1,x * 1) #:: loop(i + 1)
     loop(1)
   }
   var seen: Set[Color] = Set()
