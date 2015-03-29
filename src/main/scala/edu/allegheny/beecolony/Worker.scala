c3900246d2d8f8ba2fca9a974787f16c7fe24485
Hawk Weisman <hi@hawkweisman.me>
2015-03-29 17:46:21 -0400

    Misc work on Worker program

--- src/main/scala/edu/allegheny/beecolony/Worker.scala
+++ src/main/scala/edu/allegheny/beecolony/Worker.scala
@@ -1,11 +1,13 @@
 package edu.allegheny.beecolony
 
+import java.io.ObjectInputStream
 import java.net.Socket
 
 import scala.collection.mutable
 
 /**
  * Created by hawk on 3/29/15.
+ * @author Hawk Weisman <hi@hawkweisman.me>
  */
 object Worker extends App with Robot{
   val scoutIP = "10.0.0.36" // the Scout robot's IP address
@@ -14,4 +16,24 @@ object Worker extends App with Robot{
   override def socket: Socket = retry(new Socket(scoutIP, port))
 
   val queue = mutable.Queue[Coordinate]()
+
+  val input = socket.getInputStream
+  val deserialize = new ObjectInputStream(input)
+
+  Stream continually{
+    // apparently this is better than `while (true)`,
+    // at the very least it does not upset the code style deal.
+    // TODO: make it stop some way other than the user holding down the quit button
+    queue.enqueue(
+      // TODO: consider using a typesafe object io library
+      // but maybe adding external dependencies is bad since
+      // the robot does not have a lot of memory
+      deserialize.readObject().asInstanceOf[Coordinate]
+    )
+    goTo(queue dequeue())
+  }
+  deserialize.close()
+  input.close() // TODO: write cleanly function or something
+  socket.close() // with-resource monad? IDK.
+
 }
